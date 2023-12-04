db9ba28c3d2baade22bc98df2b5d8c91eb23ea16
Gianfranco Iacono <iacono.gian13@gmail.com>
2023-12-04 00:21:21 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# E2: Métodos de Array
+
+Bienvenidos a la Pizzería de Nucba!🍕
+
+Como nos gusta mezclar nuestras 2 pasiones que son las pizzas y la programación, decidimos que en la E2 del módulo de Javascript vamos a trabajar combinando ambas.
+
+👉 En el archivo adjunto les dejamos la base para realizar los ejercicios. En ella ya tienen el index.html con el archivo de javascript vinculado, y en el mismo tendrán un array de pizzas.
+👉 Cada "Pizza" es un objeto que tendrá definido un id (1,2,3,etc), nombre, ingredientes(será un array de ingredientes) y su precio.
+
+Nota: Pueden modificar los valores(por ejemplo, los precios) para saber si sus soluciones funcionan correctamente
+
+¿Que debén hacer?
+
+🔥 Utilizando métodos de array e iterando sobre el array de pizzas, realizar las siguientes actividades, imprimiendo en consola:
+
+- a Las pizzas que tengan un id impar.
+- b Responder: ¿Hay alguna pizza que valga menos de $600?
+- c El nombre de cada pizza con su respectivo precio.
+- d Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
+
+TODAS las respuestas deben ser USER-FRIENDLY.
+Si (como en el punto B), la respuesta es un booleano (true o false, hay o no hay), no imprimir el booleano , imprimir en consola una respuesta que toda persona pueda entender, sepa o no de programación (Es decir, no podemos imprimir un array o un objeto en consola, por ejemplo).
+
+Manejemos cada respuesta, pensando en que un usuario promedio va a leer eso.
+
+Un ejemplo de lo que sería una repuesta "user-friendly": "La pizza X, tiene un valor de $XXXX”. 💸
---
+++ index.js
@@ -0,0 +1,121 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+const hayElementosEnArray = (arr) => {
+  return arr.length === 0;
+};
+const listaDePizzasVacia = (arr) => {
+  if (hayElementosEnArray(arr)) {
+    console.log("No hay pizzas disponibles.");
+    return;
+  }
+};
+
+const imprimirPizzasIDImpar = (pizzas) => {
+  listaDePizzasVacia(pizzas);
+
+  pizzas.forEach((pizza) => {
+    if (pizza.id % 2 !== 0) {
+      console.log(
+        pizza.nombre + "\na ",
+        pizza.precio,
+        "\nIngredientes: ",
+        pizza.ingredientes.join(", ")
+      );
+    }
+  });
+};
+console.log("a) Imprimir pizzas que tengan id impar");
+imprimirPizzasIDImpar(pizzas);
+
+const hayPizzasConValorMenorASeisientos = (pizzas) => {
+  listaDePizzasVacia(pizzas);
+
+  const pizzasCumplenCondicion = pizzas.filter((pizza) => pizza.precio < 1600);
+  const strNombrePizzasQueCumplenCondiciona = pizzasCumplenCondicion
+    .map((pizzas) => pizzas.nombre)
+    .join("\n- ");
+
+  if (pizzasCumplenCondicion.length > 0) {
+    console.log(
+      "Si hay pizas que sean menor a 600, tales como \n-",
+      strNombrePizzasQueCumplenCondiciona
+    );
+  } else {
+    console.log("No existen pizzas que cumplan con esta condicion");
+  }
+};
+console.log("\n");
+console.log("b) Existen pizza con valor menor a 600?");
+hayPizzasConValorMenorASeisientos(pizzas);
+
+const imprimirNombreYPrecioDeCadaPizza = (pizzas) => {
+  listaDePizzasVacia(pizzas);
+
+  const pizzaInfo = pizzas.map((pizza) => `- ${pizza.nombre}: ${pizza.precio}`);
+  console.log(pizzaInfo.join("\n"));
+};
+console.log("\n");
+console.log("c) Imprimir el nombre de cada pizza con su respectivo precio.");
+imprimirNombreYPrecioDeCadaPizza(pizzas);
+
+const imprimirIngredietnesDeCadaPizza = (pizzas) => {
+  listaDePizzasVacia(pizzas);
+
+  pizzas.forEach((pizza) => {
+    const ingredientesDePizza = pizza.ingredientes.join("\n -");
+    console.log(
+      `Ingredientes de la ${pizza.nombre}:\n -${ingredientesDePizza}`
+    );
+  });
+};
+console.log("\n");
+console.log("d) Todos los ingredientes de cada pizza ");
+imprimirIngredietnesDeCadaPizza(pizzas);
